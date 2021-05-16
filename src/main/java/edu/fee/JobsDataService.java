979924b6aab13d65ba30c9ec1897bf012b68d1b3
Hossam Mostafa <hossamdash2@gmail.com>
2021-05-16 21:24:56 +0200

    try some stuff

--- src/main/java/edu/fee/JobsDataService.java
+++ src/main/java/edu/fee/JobsDataService.java
@@ -1,34 +1,37 @@
 package edu.fee;
 
 import java.util.List;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
 public class JobsDataService {
 
-    public static void filterJobsByTitle(List<JobDetails> jobs) {
-        countFrequencies(jobs, new Function<JobDetails,String>(){
+    private interface AtterSupplier {
+        String getAtter(JobDetails from);
+    }
 
-            @Override
-            public String apply(JobDetails t) {
-                return t.getTitle();
-            }
-            
+    public static void filterJobsByTitle(List<JobDetails> jobs) {
+        // countFrequencies(jobs, new Function<JobDetails,String>(){
+
+        // @Override
+        // public String apply(JobDetails t) {
+        // return t.getTitle();
+        // }
+
+        // });
+        countFrequencies(jobs, (JobDetails job) -> {
+            return job.getTitle();
         });
     }
 
     public static void filterJobsByCountry(List<JobDetails> jobs) {
+
     }
 
     public static void filterJobsByLevel(List<JobDetails> jobs) {
 
     }
-
-    // public static void filterJobs(List<JobDetails> jobs) {
-    // public static
-    // }
 
     public static void countFrequencies(List<JobDetails> jobs, Function<JobDetails, String> getter) {
         // hashmap to store the frequency of element
